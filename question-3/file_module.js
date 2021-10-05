8d81acbaa1aaa37e411932f46a57079cb7b9e757
Vatsal Bhimani <vatsal.bhimani@georgebrown.ca>
2021-10-05 15:30:45 -0400

    file_module.js file updated in question-3 folder

--- question-3/file_module.js
+++ question-3/file_module.js
@@ -5,18 +5,23 @@ if (!fs.existsSync(path)) {
     // Create Log files
     fs.mkdirSync(path)
     process.chdir(path)
+    console.log("------------------------------Question 3-----------------------------")
     for (let i = 0; i < 10; i++) {
         var name = `log${i}.txt`
         fs.writeFileSync(name, name)
         console.log("create files...", name)
     }
+    console.log("----------------------------------------------------------------------")
+
 } else if(fs.existsSync(path)){
     // Remove Log files
     const file = fs.readdirSync(path)
+    console.log("------------------------------Question 3-----------------------------")
     for (name of file) {
         fs.unlinkSync(`${path}/${name}`)
         console.log("delete files...", name)
     }
+    console.log("----------------------------------------------------------------------")
     fs.rmdirSync(path)
    
 }
